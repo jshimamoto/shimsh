9b30f129df0543ea42388b045243ddc50c6ab4fb
jshimamoto <joseph.shimamoto@gmail.com>
2025-04-20 22:18:56 -0700

    working shell

--- main.c
+++ main.c
@@ -7,23 +7,85 @@
 #define MAX_LINE 1024
 #define MAX_ARGS 64
 
-void parse_input(char *)
+char *read_line(void) {
+    char *line = NULL;
+    size_t buffersize = 0;
+    getline(&line, &buffersize, stdin);
+    return line;
+}
 
-void modify_value(int *p) {
-    *p = 100;
+char **parse_line(char *line) {
+    int token_buffer_size = MAX_ARGS;
+    char **tokens = malloc(token_buffer_size * sizeof(char *));
+    char *token;
+
+    if (!tokens) {
+        fprintf(stderr, "shimsh allocation error \n");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, " \t\r\n");
+    int index = 0;
+    while (token != NULL) {
+        tokens[index++] = token;
+
+        if (index >= token_buffer_size) {
+            token_buffer_size += MAX_ARGS;
+            tokens = realloc(tokens, token_buffer_size * sizeof(char *));
+            if (!tokens) {
+                fprintf(stderr, "shimsh allocation error \n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        token = strtok(NULL, " \t\r\n");
+    }
+
+    tokens[index] = NULL;
+    return tokens;
+}
+
+int execute(char **args) {
+    pid_t pid, wpid;
+    int status;
+
+    if (args[0] == NULL) {
+        return 1;
+    }
+
+    pid = fork();
+    if (pid == 0) {
+        if (execvp(args[0], args) == -1) {
+            perror("shimsh");
+        }
+    } else if (pid < 0) {
+        perror("shimsh");
+    } else {
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    return 1;
+}
+
+void loop(void) {
+    char *line;
+    char **args;
+    int status;
+
+    do {
+        printf("> ");
+        line = read_line();
+        args = parse_line(line);
+        status = execute(args);
+
+        free(line);
+        free(args);
+    } while (status);
 }
 
 int main() {
-    int a;
-
-    printf("Enter an integer: ");
-    scanf("%d", &a);
-
-    printf("It is currently %d\n", a);
-
-    modify_value(&a);
-
-    printf("Now it is %d\n", a);
-
+    loop();
     return 0;
 }
